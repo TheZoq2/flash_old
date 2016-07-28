9dfe40a498052152fd82ae56bd7ac8fa2012b16a
Frans Skarman <frans.skarman@gmail.com>
2016-07-28 17:36:40 +0200

    Add get_images function

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,61 @@
+#![allow(dead_code)]
 
 extern crate gtk;
+extern crate glob;
 
 use gtk::prelude::*;
 
-use gtk::{Button, Window, WindowType, Image};
+use gtk::{Window, WindowType, Image};
 
 use std::path::{Path, PathBuf};
 
 use glob::glob;
 
-fn get_images_in_directory(path: &Path) -> Vec<PathBuf>
+fn get_images_in_directory(path: String) -> Vec<PathBuf>
 {
-    for path in glob.
+    let mut result = Vec::<PathBuf>::new();
+
+    let full_path = path + "/*";
+
+    for entry in glob(&full_path).expect("Failed to read glob")
     {
-        
+        match entry
+        {
+            Ok(path) => result.push(path),
+            Err(e) => println!("{}", e)
+        }
     }
+
+    result
 }
 
-fn main() {
+fn main() 
+{
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
-
+    
     let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK+ Program");
+    window.set_title("Flash");
     window.set_default_size(350, 70);
     //let button = Button::new_with_label("Click me!");
     let image = Image::new_from_file("media/test.png");
     window.add(&image);
     window.show_all();
 
+    window.
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
+
+    let paths = get_images_in_directory(String::from("/tmp"));
+    
+    for path in paths
+    {
+        println!("{}", path.display());
+    }
 
     gtk::main();
 }
